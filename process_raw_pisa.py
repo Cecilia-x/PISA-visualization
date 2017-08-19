8a187f9c7a50d886ec76afbdba52e0c0e82b3e22
Cecilia-x <30399351+Cecilia-x@users.noreply.github.com>
2017-08-19 19:13:38 +0800

    Update process_raw_pisa.py

--- process_raw_pisa.py
+++ process_raw_pisa.py
@@ -10,16 +10,15 @@ cols = ['CNT', 'SUBNATIO', 'STRATUM', 'OECD', 'NC', 'SCHOOLID', 'STIDSTD', 'ESCS
 # Read pisa file.
 df = pandas.read_csv("pisa2012.csv", index_col=False, usecols=cols, encoding='utf-8')
 
-# Select useful column
+# Melt columns of test types into 2 columns: PVTYPE, SCORE
 pvs = ['PV1MATH', 'PV2MATH', 'PV3MATH', 'PV4MATH', 'PV5MATH', 
        'PV1READ', 'PV2READ', 'PV3READ', 'PV4READ', 'PV5READ', 
        'PV1SCIE', 'PV2SCIE', 'PV3SCIE', 'PV4SCIE', 'PV5SCIE']
 iv = ['CNT', 'SCHOOLID', 'ESCS', 'W_FSTUWT']
-df = df[PVS+iv]
-
-# Split subject and test type and create new column
 df2 = df.melt(id_vars=iv, value_vars=pvs,
                 var_name='PVTYPE', value_name='SCORE')
+
+# Split subject and test type and create new column
 df2 = df2.assign(PVNUM=df2['PVTYPE'].apply(lambda x: x[2]), SUBJECT=df2['PVTYPE'].apply(lambda x: x[3:]))
 
 # Function for grouping by country include coef between ESCS and performance.
