bfa3132b0817f1e0ee77b6026faa1dc754bc9603
Cecilia-x <30399351+Cecilia-x@users.noreply.github.com>
2017-08-19 19:11:22 +0800

    Create process_raw_pisa.py

---
+++ process_raw_pisa.py
@@ -0,0 +1,50 @@
+import pandas
+import numpy
+import statsmodels.formula.api as sm
+
+cols = ['CNT', 'SUBNATIO', 'STRATUM', 'OECD', 'NC', 'SCHOOLID', 'STIDSTD', 'ESCS', 'W_FSTUWT',
+        'PV1MATH', 'PV2MATH', 'PV3MATH', 'PV4MATH', 'PV5MATH', 
+        'PV1READ', 'PV2READ', 'PV3READ', 'PV4READ', 'PV5READ', 
+        'PV1SCIE', 'PV2SCIE', 'PV3SCIE', 'PV4SCIE', 'PV5SCIE']
+        
+# Read pisa file.
+df = pandas.read_csv("pisa2012.csv", index_col=False, usecols=cols, encoding='utf-8')
+
+# Select useful column
+pvs = ['PV1MATH', 'PV2MATH', 'PV3MATH', 'PV4MATH', 'PV5MATH', 
+       'PV1READ', 'PV2READ', 'PV3READ', 'PV4READ', 'PV5READ', 
+       'PV1SCIE', 'PV2SCIE', 'PV3SCIE', 'PV4SCIE', 'PV5SCIE']
+iv = ['CNT', 'SCHOOLID', 'ESCS', 'W_FSTUWT']
+df = df[PVS+iv]
+
+# Split subject and test type and create new column
+df2 = df.melt(id_vars=iv, value_vars=pvs,
+                var_name='PVTYPE', value_name='SCORE')
+df2 = df2.assign(PVNUM=df2['PVTYPE'].apply(lambda x: x[2]), SUBJECT=df2['PVTYPE'].apply(lambda x: x[3:]))
+
+# Function for grouping by country include coef between ESCS and performance.
+def f_c(group):
+    s = group['SCORE']
+    e = group['ESCS']
+    w = group['W_FSTUWT']
+    #apply the weighted mean.
+    savg = (s*w).sum()/w.sum()
+    eavg = (e*w).sum()/w.sum()
+    cnt = s.count()
+    if not e.isnull().all():
+        lm = sm.ols(formula="SCORE ~ ESCS", data=group, missing='drop').fit()
+        coef = lm.params.ESCS
+        rsqured = lm.rsquared
+    else:
+        coef = None
+        rsqured = None
+    return pandas.DataFrame({'MEANSCORE':savg, 'MEANESCS':eavg, 'COUNT': cnt, 'COEF':coef, 'RSQURED':rsqured}, index=[0])
+
+# Group by country and subject.
+grouped = df2.groupby(['CNT','SUBJECT']).apply(f_c)
+
+# Remove null rows (include Albania data).
+grouped = g2[g2.COEF.notnull()]
+
+grouped.to_csv("group_by_country.csv")
+
